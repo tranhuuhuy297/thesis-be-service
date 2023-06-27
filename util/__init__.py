becf253ab608d4df39da4b88689ddd51d9c25826
tranhuuhuy297 <tranhuuhuy297@gmail.com>
2023-06-27 16:27:18 +0700

    add generate image

--- util/__init__.py
+++ util/__init__.py
@@ -2,10 +2,13 @@ from util.pinecone import Pinecone
 from util.sqs import SQS
 from dotenv import load_dotenv
 from util.s3_util import S3
-from util.const_util import AWS_SQS_NAME
+from util.const_util import AWS_SQS_NAME, AWS_SQS_GENERATE
 
 load_dotenv(dotenv_path='.env')
 
 sqs = SQS(queue_name=AWS_SQS_NAME)
+sqs_generate_image = SQS(queue_name=AWS_SQS_GENERATE)
+
 s3_image = S3(bucket_name='image')
+
 pinecone = Pinecone()

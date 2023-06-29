07150e7810559a7a7aef9b9e6f441c03aab076c9
tranhuuhuy297 <tranhuuhuy297@gmail.com>
2023-06-29 13:19:12 +0700

    add dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM python:3.8.9-slim
+FROM python:3.10.12-slim
+
+RUN apt-get -y update && apt-get install -y libzbar-dev
 
 WORKDIR /app
 COPY . ./
--- util/__init__.py
+++ util/__init__.py
@@ -1,10 +1,7 @@
 from util.pinecone import Pinecone
 from util.sqs import SQS
-from dotenv import load_dotenv
 from util.s3_util import S3
 from util.const_util import AWS_SQS_NAME, AWS_SQS_GENERATE
-
-load_dotenv(dotenv_path='.env')
 
 sqs = SQS(queue_name=AWS_SQS_NAME)
 sqs_generate_image = SQS(queue_name=AWS_SQS_GENERATE)

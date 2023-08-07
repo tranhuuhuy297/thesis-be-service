56f9c0980b1dca957bec1ce37aa3b2d1cb4b0902
tranhuuhuy297 <tranhuuhuy297@gmail.com>
2023-08-07 11:29:35 +0700

    update prompt

--- util/langchain.py
+++ util/langchain.py
@@ -2,12 +2,15 @@ from util.const_util import OPENAI_API_KEY
 from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
 
-llm = OpenAI(model_name='gpt-3.5-turbo', openai_api_key=OPENAI_API_KEY, temperature=0.5)
+llm = OpenAI(model_name='gpt-3.5-turbo',
+             openai_api_key=OPENAI_API_KEY, temperature=0.5)
 
 prompt = PromptTemplate.from_template(
     "you are an artist, a writer, a photographer. \
-    I will give you some words, they are  {subjects}. \
-    You have to combine those words in english and imagine to describe me a creative picture. \
+    I will give you some words, they are {subjects}. \
+    First, you must translate these words {subjects} into English. \
+    I call the words you have translated into English 'A'. \
+    You have to utilize 'A' and imagine to describe me a creative picture. \
     Your sentence should be simple, succinct, detail, max in 60 words, use collective nouns. \
     Your picture should has many subjects for detail. \
     You have to give me 10 creative sentences. \
